699522d56c6692aa0c390e679d7b48b6dd147dff
Blerta <bkrasniqi97@gmail.com>
2023-11-05 22:13:30 +0000

    addEventListener to log click on button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,33 +16,44 @@ function setTimeAndQuestions() {
     if (timeLeft === 0) {
       clearInterval(timerInterval);
     }
-  }, 1000)
+  }, 1000);
 
   start.setAttribute("style", "display: none");
   hiddenQuestions.classList.remove("hide");
-  
-  for (var i = 0; i < questions.length; i++) {
-    askQuestions.textContent = questions[i].question;
-    
-    // for loop for answer array //
-    var data = questions[i].answers;
-    for (var j = 0; j < data.length; j++) {
-      const answerButton = document.createElement("button");
-      answerButton.textContent = j + 1 + ". " + data[j];
-      choices.appendChild(answerButton);
+  // first question
+  var index = 0;
+  // questions[i].question
+  // click on button to bring up next question
+  // for (var i = 0; i < questions.length; i++) {
+  askQuestions.textContent = questions[index].question;
 
-    
-    }
-  
+  // for loop for answer array //
+  var data = questions[index].answers;
+  for (var j = 0; j < data.length; j++) {
+    const answerButton = document.createElement("button");
+    answerButton.textContent = j + 1 + ". " + data[j];
+    choices.appendChild(answerButton);
   }
-console.log(orderedList)
- 
-};
+  // addEventListener to log clicks on the buttons and increase index - checking choices div, therefore need function to check if button is clicked and not rest of div
+  choices.addEventListener("click", (event) => {
+    const isButton = event.target.nodeName === "BUTTON";
+    if (!isButton) {
+      return;
+    }
+    index = index + 1;
+    console.log(index);
+    // change the text content and answers to the next question
+    askQuestions.textContent = questions[index].question;
+    data = questions[index].answers;
+    var allButtons = document.querySelectorAll("button"); // selects all buttons that we created in the for loop 
+    for (var j = 0; j < data.length; j++) {
+      allButtons[j].textContent = j + 1 + ". " + data[j];
+    }
+  });
+}
+
 // setTime only when start quiz button is pressed
 button.addEventListener("click", setTimeAndQuestions);
-
-
-
 
 // iterate over the list of questions
 // if a user clicked on an answer
